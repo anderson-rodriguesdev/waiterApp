4c55b76d2f94d2167ce8a7e71edcff4215a58e05
Anderson Rodrigues <andersonrodrigues.silva96@gmail.com>
2022-12-27 19:45:02 -0300

    first commit - api creation/schemas/router

---
+++ api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017")
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`😘 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(`Deu zica, se liga: ${error}`));
